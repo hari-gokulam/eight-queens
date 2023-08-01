bfab9007def4dfcda2e62b4b8fdd31d215809cc5
hari-gokulam <139342259+hari-gokulam@users.noreply.github.com>
2023-08-01 07:53:27 +0000

    .

---
+++ src/webassembly/pyodide/scripts.js
@@ -0,0 +1,63 @@
+function createBoard(boardId) {
+    const boardElement = document.getElementById('board');
+    const boardFragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            const sId = boardId + i + j;
+            const bg = (i + j) % 2 === 0 ? 'white' : 'black';
+            const cellDiv = document.createElement('div');
+            cellDiv.id = sId;
+            cellDiv.className = `cell ${bg}`;
+            boardFragment.appendChild(cellDiv);
+        }
+    }
+
+    const chessboardDiv = document.createElement('div');
+    chessboardDiv.id = boardId;
+    chessboardDiv.className = 'chessboard';
+    chessboardDiv.appendChild(boardFragment);
+
+    boardElement.appendChild(chessboardDiv);
+}
+
+function addQueen(boardId, r, c) {
+    const cellId = boardId + r + c;
+    const elem = document.getElementById(cellId);
+    elem.innerHTML = '&#9819;'; //'♕'; // Using a queen chess symbol instead of the HTML entity.
+}
+async function fetchAndLoadPythonScript(pyodide) {
+    console.log("Fetching Python script...");
+
+    const response = await pyodide.runPythonAsync(`
+        from pyodide.http import pyfetch
+        response = await pyfetch('/chess.py')
+        with open('chessscript.py', "wb") as f:
+            f.write(await response.bytes())
+    `);
+
+    console.log("Python script fetched and loaded successfully.");
+}
+
+async function runPython() {
+    console.log("runPython");
+
+    const pyodide = await loadPyodide();
+    await fetchAndLoadPythonScript(pyodide);
+
+
+    pkg = pyodide.pyimport("chessscript")
+    const results = pkg.get_positions();
+    //const results = await pyodide.runPythonAsync("chessscript.get_positions()");
+    const resArray = JSON.parse(results.toString()); //pyodide.toJs(results);
+
+    for (const [index, solution] of resArray.entries()) {
+        const boardId = 'sol' + index;
+        createBoard(boardId);
+
+        for (const [x, y] of solution.entries()) {
+            addQueen(boardId, x, y);
+        }
+    }
+}
+
